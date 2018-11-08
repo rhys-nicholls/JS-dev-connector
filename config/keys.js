55f30787477ca6cb67c8285da1b40cbb87453bb8
rhys-nicholls <34071702+rhys-nicholls@users.noreply.github.com>
2018-11-08 13:49:51 +0000

    Added login route. Includes generation of JWT

--- config/keys.js
+++ config/keys.js
@@ -1,4 +1,4 @@
 module.exports = {
-  mongoURI:
-    'mongodb://rhys:frankie1988@ds153763.mlab.com:53763/devconnectorproject',
+  mongoURI: 'mongodb://rhys:frankie1988@ds153763.mlab.com:53763/devconnectorproject',
+  secret: 'secret',
 };
--- routes/api/users.js
+++ routes/api/users.js
@@ -1,6 +1,9 @@
+/* eslint-disable consistent-return */
 const express = require('express');
 const gravatar = require('gravatar');
 const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+const keys = require('../../config/keys');
 
 // Load User model
 const User = require('../../models/User');
@@ -17,7 +20,6 @@ router.get('/test', (req, res) => res.json({ msg: 'Users Works' }));
 // @access  Public
 router.post('/register', (req, res) => {
   User.findOne({ email: req.body.email })
-    // eslint-disable-next-line consistent-return
     .then((user) => {
       if (user) return res.status(400).json({ email: 'Email already exists' });
 
@@ -46,6 +48,37 @@ router.post('/register', (req, res) => {
             .catch(jsonError => console.log(jsonError));
         });
       });
+    });
+});
+
+// @route   GET api/users/login
+// @desc    Login user / Return JWT token
+// @access  Public
+router.post('/login', (req, res) => {
+  const formData = req.body;
+
+  // Find user by email
+  User.findOne({ email: formData.email })
+    .then((user) => {
+      if (!user) return res.status(404).json({ email: 'User not found' });
+
+      bcrypt.compare(formData.password, user.password)
+        .then((isMatch) => {
+          // Create JWT payload
+          const payload = {
+            id: user.id,
+            name: user.name,
+            avatar: user.avatar,
+          };
+
+          if (isMatch) {
+            jwt.sign(payload, keys.secret, { expiresIn: 3600 }, (err, token) => {
+              res.json({ success: true, token: `Bearer ${token}` });
+            });
+          } else {
+            return res.status(400).json({ password: 'Password Incorrect' });
+          }
+        });
     });
 });
 
