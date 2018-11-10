5b94f73204ff8fde4aed3b9973b139ccd1a4ede6
rhys-nicholls <34071702+rhys-nicholls@users.noreply.github.com>
2018-11-10 14:23:01 +0000

    Created Post model

---
+++ models/Post.js
@@ -0,0 +1,59 @@
+/* eslint-disable prefer-destructuring */
+const mongoose = require('mongoose');
+
+const Schema = mongoose.Schema;
+
+// Create Schema
+const PostSchema = new Schema({
+  user: {
+    type: Schema.Types.ObjectId,
+    ref: 'users',
+  },
+  text: {
+    type: String,
+    required: true,
+  },
+  name: {
+    type: String,
+  },
+  avatar: {
+    type: String,
+  },
+  date: {
+    type: Date,
+    default: Date.now,
+  },
+  likes: [
+    {
+      user: {
+        type: Schema.Types.ObjectId,
+        ref: 'users',
+      },
+    },
+  ],
+  comments: [
+    {
+      user: {
+        type: Schema.Types.ObjectId,
+        ref: 'users',
+      },
+      text: {
+        type: String,
+        required: true,
+      },
+      name: {
+        type: String,
+      },
+      avatar: {
+        type: String,
+      },
+      date: {
+        type: Date,
+        default: Date.now,
+      },
+    },
+  ],
+});
+
+const Post = mongoose.model('profile', PostSchema);
+module.exports = Post;
