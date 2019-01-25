23bc5a5453255a89f90b0f695a72cbc7af608ca4
rhys-nicholls <34071702+rhys-nicholls@users.noreply.github.com>
2019-01-25 16:25:16 +0000

    Made login form functional. Added login permanace

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,11 @@
 import React, { Component } from 'react';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
 import { Provider } from 'react-redux';
+import jwt_decode from 'jwt-decode';
 
 import store from './store';
+import setAuthToken from './utils/setAuthToken';
+import { setCurrentUser } from './actions/authActions';
 
 import Navbar from './components/layout/Navbar';
 import Footer from './components/layout/Footer';
@@ -12,21 +15,32 @@ import Login from './components/auth/Login';
 
 import './App.css';
 
+//Check for Auth Token
+if (localStorage.jwtToken) {
+  // Set auth token header
+  setAuthToken(localStorage.jwtToken);
+  // Decode token and get user info
+  const decoded = jwt_decode(localStorage.jwtToken);
+  // Set user and isAuthenticated
+  store.dispatch(setCurrentUser(decoded));
+}
+
+
 class App extends Component {
   render() {
     return (
-      <Provider store={ store }>
-          <Router>
-            <div className="App">
+      <Provider store={store}>
+        <Router>
+          <div className="App">
             <Navbar />
-            <Route exact path='/' component={ Landing } />
+            <Route exact path='/' component={Landing} />
             <div className='container'>
-              <Route path='/login' component={ Login } />
-              <Route path="/register" component={ Register } />
+              <Route path='/login' component={Login} />
+              <Route path="/register" component={Register} />
             </div>
             <Footer />
           </div>
-        </Router>     
+        </Router>
       </Provider>
     );
   }
