edf35015040263f4e64a25034b66d2bd68975dda
rhys-nicholls <34071702+rhys-nicholls@users.noreply.github.com>
2019-02-06 16:23:24 +0000

    Added Spinner component to be used for data loading

--- client/src/components/dashboard/Dashboard.js
+++ client/src/components/dashboard/Dashboard.js
@@ -2,18 +2,48 @@ import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 import { getCurrentProfile } from '../../actions/profileActions';
+import Spinner from '../common/Spinner';
 
 class Dashboard extends Component {
   componentDidMount() {
     this.props.getCurrentProfile();
   }
   render() {
+    const { user } = this.props.auth;
+    const { profile, loading } = this.props.profile;
+
+    let dashboardContent;
+
+    if(profile === null || loading) {
+      dashboardContent = <Spinner />
+    } else {
+      dashboardContent = <h1>Hello</h1>
+    }
     return (
-      <div>
-        <h1>Dashboard</h1> 
+      <div className="dashboard">
+        <div className="container">
+          <div className="row">
+            <div className="col-md-12">
+              <h1 className="display-4">Dashboard</h1>
+              {dashboardContent} 
+            </div>  
+          </div>
+        </div>
       </div>
     )
   }
 }
 
-export default connect(null, {getCurrentProfile})(Dashboard);
+const mapStateToPRope = state => ({
+  profile: state.profile,
+  auth: state.auth
+})
+
+
+Dashboard.protoTypes = {
+  getCurrentProfile: PropTypes.func.isRequired,
+  auth: PropTypes.object.isRequired,
+  profile: PropTypes.object.isRequired  
+}
+
+export default connect(mapStateToPRope, {getCurrentProfile})(Dashboard);
