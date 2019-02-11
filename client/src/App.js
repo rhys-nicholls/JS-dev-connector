b5ecb13cdbd8ee96857e55aaf2226c125c7e6d25
rhys-nicholls <34071702+rhys-nicholls@users.noreply.github.com>
2019-02-11 15:11:40 +0000

    Added CreateProfile component and route

--- client/src/App.js
+++ client/src/App.js
@@ -15,7 +15,8 @@ import Footer from './components/layout/Footer';
 import Landing from './components/layout/Landing';
 import Register from './components/auth/Register';
 import Login from './components/auth/Login';
-import Dashboard from './components/dashboard/Dashboard'
+import Dashboard from './components/dashboard/Dashboard';
+import CreateProfile from './components/create-profile/CreateProfile';
 
 import './App.css';
 
@@ -54,6 +55,9 @@ class App extends Component {
               <Route exact path="/register" component={Register} />
               <Switch>
                 <PrivateRoute exact path="/dashboard" component={Dashboard} />
+              </Switch>
+              <Switch>
+                <PrivateRoute exact path="/create-profile" component={CreateProfile} />
               </Switch>
             </div>
             <Footer />
