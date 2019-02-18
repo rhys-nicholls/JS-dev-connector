e46374e2d3cc90a3260496a5d9c5953f4cea8df8
rhys-nicholls <34071702+rhys-nicholls@users.noreply.github.com>
2019-02-18 16:51:18 +0000

    Added EditProfile component

--- client/src/components/layout/Navbar.js
+++ client/src/components/layout/Navbar.js
@@ -1,3 +1,4 @@
+/* eslint-disable jsx-a11y/anchor-is-valid */
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
 import PropTypes from 'prop-types';
@@ -17,6 +18,11 @@ class Navbar extends Component {
 
     const authLinks = (
       <ul className="navbar-nav ml-auto">
+        <li className="nav-item">
+        <Link className="nav-link" to="/dashboard">
+        Dashboard
+        </Link>
+        </li>
         <li className="nav-item">
           <a
             href=""
