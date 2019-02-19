30db0c632315eec4ce6ebc471e74633c49e77ab1
rhys-nicholls <34071702+rhys-nicholls@users.noreply.github.com>
2019-02-19 16:15:19 +0000

    Added AddExperience component functionailty

--- client/src/actions/profileActions.js
+++ client/src/actions/profileActions.js
@@ -1,7 +1,13 @@
 import axios from 'axios';
 import { logoutUser } from './authActions';
 
-import { GET_PROFILE, PROFILE_LOADING, GET_ERRORS, CLEAR_CURRENT_PROFILE, SET_CURRENT_USER } from './types';
+import {
+  GET_PROFILE,
+  PROFILE_LOADING,
+  GET_ERRORS,
+  CLEAR_CURRENT_PROFILE,
+  SET_CURRENT_USER
+} from './types';
 
 // Get current profile
 export const getCurrentProfile = () => dispatch => {
@@ -22,29 +28,46 @@ export const getCurrentProfile = () => dispatch => {
     );
 };
 
-// Delete account profile
-export const deleteAccount = () => dispatch => {
-  if(window.confirm('Are you you sure you want to dete your account')) {
-    axios.delete('/api/profile')
-    .then(res => 
-      dispatch(logoutUser())
-    ).catch(err => 
+// Add Experience
+export const addExperience = (expData, history) => dispatch => {
+  axios
+    .post('/api/profile/experience', expData)
+    .then(res => history.push('/dashboard'))
+    .catch(err =>
       dispatch({
         type: GET_ERRORS,
         payload: err.response.data
-      }))
+      })
+    );
+};
+
+// Delete account profile
+export const deleteAccount = () => dispatch => {
+  if (window.confirm('Are you you sure you want to dete your account')) {
+    axios
+      .delete('/api/profile')
+      .then(res => dispatch(logoutUser()))
+      .catch(err =>
+        dispatch({
+          type: GET_ERRORS,
+          payload: err.response.data
+        })
+      );
   }
-}
+};
 
 // Create profile
 export const createProfile = (profileData, history) => dispatch => {
-  axios.post('/api/profile', profileData)
-  .then(res => history.push('/dashboard'))
-  .catch(err => dispatch({
-    type: GET_ERRORS,
-    payload: err.response.data
-  }))
-}
+  axios
+    .post('/api/profile', profileData)
+    .then(res => history.push('/dashboard'))
+    .catch(err =>
+      dispatch({
+        type: GET_ERRORS,
+        payload: err.response.data
+      })
+    );
+};
 
 // Profile loading
 export const setProfileLoading = () => {
