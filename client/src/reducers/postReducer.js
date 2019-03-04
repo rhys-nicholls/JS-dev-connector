c4d4de2f3a15a3479f61867641c7d2acfa724221
rhys-nicholls <34071702+rhys-nicholls@users.noreply.github.com>
2019-03-04 14:09:16 +0000

    Setup Post state and reducer. Added first action, addPost

---
+++ client/src/reducers/postReducer.js
@@ -0,0 +1,12 @@
+const intialState = {
+  posts: [],
+  post: {},
+  loading: false
+};
+
+export default function(state = intialState, action) {
+  switch (action.type) {
+    default:
+      return state;
+  }
+}
