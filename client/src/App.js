3e57f16bc8419fcc3421932ea148040a8b2843d7
rhys-nicholls <34071702+rhys-nicholls@users.noreply.github.com>
2019-03-08 14:34:04 +0000

    Added single Post component

--- client/src/App.js
+++ client/src/App.js
@@ -25,6 +25,7 @@ import Profiles from './components/profiles/Profiles';
 import Profile from './components/profile/Profile';
 import NotFound from './components/not-found/NotFound';
 import Posts from './components/posts/Posts';
+import Post from './components/post/Post';
 
 import './App.css';
 
@@ -99,6 +100,13 @@ class App extends Component {
                   exact
                   path="/feed"
                   component={Posts}
+                />
+              </Switch>
+              <Switch>
+                <PrivateRoute
+                  exact
+                  path="/post/:id"
+                  component={Post}
                 />
               </Switch>
             </div>
