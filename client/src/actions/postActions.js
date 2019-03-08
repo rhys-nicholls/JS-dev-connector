72e6a497d02bdd216125a0fa2544a5d842b4bf47
rhys-nicholls <34071702+rhys-nicholls@users.noreply.github.com>
2019-03-08 11:57:32 +0000

    Added Delete post, like and unlike post

--- client/src/actions/postActions.js
+++ client/src/actions/postActions.js
@@ -1,5 +1,11 @@
 import axios from 'axios';
-import { ADD_POST, GET_ERRORS , GET_POSTS, POST_LOADING} from './types';
+import {
+  ADD_POST,
+  GET_ERRORS,
+  GET_POSTS,
+  POST_LOADING,
+  DELETE_POST
+} from './types';
 
 // Add post
 export const addPost = postData => dispatch => {
@@ -22,7 +28,7 @@ export const addPost = postData => dispatch => {
 // Get Posts
 export const getPosts = () => dispatch => {
   dispatch(setPostsLoading);
-  
+
   axios
     .get('/api/posts')
     .then(res =>
@@ -37,11 +43,55 @@ export const getPosts = () => dispatch => {
         payload: null
       })
     );
-}; 
+};
+
+// Delete post
+export const deletePost = id => dispatch => {
+  axios
+    .delete(`/api/posts/${id}`)
+    .then(res =>
+      dispatch({
+        type: DELETE_POST,
+        payload: id
+      })
+    )
+    .catch(err =>
+      dispatch({
+        type: GET_ERRORS,
+        payload: err.response.data
+      })
+    );
+};
+
+// Add Like
+export const addLike = id => dispatch => {
+  axios
+    .post(`/api/posts/like/${id}`)
+    .then(res => dispatch(getPosts()))
+    .catch(err =>
+      dispatch({
+        type: GET_ERRORS,
+        payload: err.response.data
+      })
+    );
+};
+
+// Remove Like
+export const removeLike = id => dispatch => {
+  axios
+    .post(`/api/posts/unlike/${id}`)
+    .then(res => dispatch(getPosts()))
+    .catch(err =>
+      dispatch({
+        type: GET_ERRORS,
+        payload: err.response.data
+      })
+    );
+};
 
 // Set loading state
 export const setPostsLoading = () => {
   return {
     type: POST_LOADING
-  }
-}
+  };
+};
